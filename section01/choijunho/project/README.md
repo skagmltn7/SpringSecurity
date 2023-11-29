edd86dbd620d9394730fefbe7a2aa43731c778a8
junhochoi-dev <cjh970422@gmail.com>
2023-11-29 16:18:56 +0900

    [ADD] section01

---
+++ section01/choijunho/project/README.md
@@ -0,0 +1,5 @@
+# SpringSecurity
+
+![image](https://github.com/junhochoi-dev/SpringBoot_Security/assets/39554558/ecd75a95-3adf-4a2d-bbe9-51378ead5bcd)
+
+https://www.inflearn.com/course/%EC%BD%94%EC%96%B4-%EC%8A%A4%ED%94%84%EB%A7%81-%EC%8B%9C%ED%81%90%EB%A6%AC%ED%8B%B0/dashboard
---
+++ section01/choijunho/project/src/main/java/com/project/springsecurity/SecurityController.java
@@ -0,0 +1,31 @@
+package com.project.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
+
+    @GetMapping("loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminPay(){
+        return "adminPay";
+    }
+}
